b0126d68d1599199c8bf058b2a8e9ca6edd97e3e
Paul Bolle <pebolle@tiscali.nl>
2016-10-27 13:20:26 +0200

    uapi/linux/fb.h: help unifdef to do the right thing
    This header exports this preprocessor check:
        #ifdef CONFIG_FB_BACKLIGHT
    
    Since CONFIG_FB_BLACKLIGHT is not valid in userspace that check will always
    evaluate to false when exported. We should convert this to:
        #if defined(CONFIG_FB_BACKLIGHT) && defined(__KERNEL__)
    
    That allows unifdef to remove the preprocessor check and the unreachable code
    it guards. That way we'll no longer leak this kconfig macro to userspace.
    
    Signed-off-by: Paul Bolle <pebolle@tiscali.nl>

--- include/uapi/linux/fb.h
+++ include/uapi/linux/fb.h
@@ -392,7 +392,7 @@ struct fb_cursor {
 	struct fb_image	image;	/* Cursor image */
 };
 
-#ifdef CONFIG_FB_BACKLIGHT
+#if defined(CONFIG_FB_BACKLIGHT) && defined(__KERNEL__)
 /* Settings for the generic backlight code */
 #define FB_BACKLIGHT_LEVELS	128
 #define FB_BACKLIGHT_MAX	0xFF
