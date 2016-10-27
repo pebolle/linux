8bcca2b8dcf2dce6129c557e31f2712a7582f960
Paul Bolle <pebolle@tiscali.nl>
2016-10-27 13:20:26 +0200

    raw: remove kconfig macro from exported header
    raw.h is exported to userspace. It contains:
        #define MAX_RAW_MINORS CONFIG_MAX_RAW_DEVS
    
    Since CONFIG_MAX_RAW_DEVS is not valid in userspace MAX_RAW_MINORS will be
    defined to 0 in userspace.
    
    It turns out the kernel can do without MAX_RAW_MINORS and use
    CONFIG_MAX_RAW_DEVS directly. That means we can hardcode MAX_RAW_MINORS to 0 in
    the header.
    
    Signed-off-by: Paul Bolle <pebolle@tiscali.nl>

--- include/uapi/linux/raw.h
+++ include/uapi/linux/raw.h
@@ -13,6 +13,10 @@ struct raw_config_request
 	__u64	block_minor;
 };
 
-#define MAX_RAW_MINORS CONFIG_MAX_RAW_DEVS
+/*
+ * Exported apparently by mistake. Keep it here to not break compilation of
+ * programs that make another mistake by using MAX_RAW_MINORS in their code.
+ */
+#define MAX_RAW_MINORS 0
 
 #endif /* __LINUX_RAW_H */
